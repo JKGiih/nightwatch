b12d461e9722416c9d45165cedacb989bfc3d998
J. K. Giih <jkgiih@nervoushorse.com>
2015-12-29 15:35:04 +0200

    Working menu, all watches

--- instructions.gd
+++ instructions.gd
@@ -1,7 +1,13 @@
 extends Panel
 
-func _ready():
-	pass
+var just_pressed
 
-func _on_goto_scene_pressed():
-        get_node("/root/global").goto_scene("res://start.xml")
+func _ready():
+	just_pressed = true
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	if (not (Input.is_action_pressed("ACCEPT"))):
+		just_pressed = false
+	if (Input.is_action_pressed("ACCEPT") and not just_pressed):
+		get_node("/root/global").goto_scene("res://start.xml")
--- start.gd
+++ start.gd
@@ -6,13 +6,13 @@ var just_pressed
 func _ready():
 	cursor = "START"
 	get_node("start").set_opacity(1)
-	just_pressed = false
+	just_pressed = true
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	if (not (Input.is_action_pressed("MOVE_UP") or Input.is_action_pressed("MOVE_DOWN"))):
+	if (not (Input.is_action_pressed("MOVE_UP") or Input.is_action_pressed("MOVE_DOWN") or Input.is_action_pressed("ACCEPT"))):
 		just_pressed = false
-	if (Input.is_action_pressed("ACCEPT")):
+	if (Input.is_action_pressed("ACCEPT") and not just_pressed):
 		if (cursor == "START"):
 			get_node("/root/global").goto_scene("res://game.xml")
 		if (cursor == "INSTRUCTIONS"):
@@ -21,7 +21,7 @@ func _fixed_process(delta):
 			get_node("/root/global").goto_scene("res://credits.xml")
 		if (cursor == "QUIT"):
 			get_tree().quit()
-	if (Input.is_action_pressed("MOVE_UP") and just_pressed == false):
+	if (Input.is_action_pressed("MOVE_UP") and not just_pressed):
 		if (cursor == "INSTRUCTIONS"):
 			get_node("instructions").set_opacity(.5)
 			get_node("start").set_opacity(1)
@@ -35,7 +35,7 @@ func _fixed_process(delta):
 			get_node("credits").set_opacity(1)
 			cursor = "CREDITS"
 		just_pressed = true
-	if (Input.is_action_pressed("MOVE_DOWN") and just_pressed == false):
+	if (Input.is_action_pressed("MOVE_DOWN") and not just_pressed):
 		if (cursor == "CREDITS"):
 			get_node("credits").set_opacity(.5)
 			get_node("quit").set_opacity(1)
